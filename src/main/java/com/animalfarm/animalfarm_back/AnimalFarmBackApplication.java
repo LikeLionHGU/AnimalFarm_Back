732392ecc77c09db27b0661726cdc4dfe0cab2ef
hyeokkiyaa <khm38607574@gmail.com>
2025-02-07 11:07:02 +0900

    init project

---
+++ src/main/java/com/animalfarm/animalfarm_back/AnimalFarmBackApplication.java
@@ -0,0 +1,13 @@
+package com.animalfarm.animalfarm_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalFarmBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalFarmBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/animalfarm/animalfarm_back/AnimalFarmBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalfarm.animalfarm_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalFarmBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
