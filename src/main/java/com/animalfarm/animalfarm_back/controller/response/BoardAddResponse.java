5e5dd7ca629cc131d9bd55ff4148d8aed1f8d700
hyeokkiyaa <khm38607574@gmail.com>
2025-02-10 19:35:22 +0900

    feat: upload board added

--- src/main/java/com/animalfarm/animalfarm_back/controller/response/BoardAddResponse.java
+++ src/main/java/com/animalfarm/animalfarm_back/controller/response/BoardAddResponse.java
@@ -1,10 +1,12 @@
 package com.animalfarm.animalfarm_back.controller.response;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 @Data
 @Builder
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardAddResponse {
     private int isLogin;
     private int isSuccess;
--- src/main/java/com/animalfarm/animalfarm_back/service/BoardService.java
+++ src/main/java/com/animalfarm/animalfarm_back/service/BoardService.java
@@ -3,13 +3,27 @@ package com.animalfarm.animalfarm_back.service;
 import com.animalfarm.animalfarm_back.controller.request.BoardAddRequest;
 import com.animalfarm.animalfarm_back.controller.response.BoardAddResponse;
 import com.animalfarm.animalfarm_back.domain.Board;
+import com.animalfarm.animalfarm_back.dto.BoardDto;
+import com.animalfarm.animalfarm_back.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
+
+    private final BoardRepository boardRepository;
+
     public BoardAddResponse saveBoard(BoardAddRequest boardAddRequest, String uploadUrl) {
-        BoardDto boardDto = BoardDto.from(boardRepository.save(Board.from(boardAddRequest, uploadUrl)));
+        BoardDto boardDto = BoardDto.fromBoard(boardRepository.save(Board.from(boardAddRequest, uploadUrl)));
+        BoardAddResponse boardAddResponse = new BoardAddResponse();
+        boardAddResponse.setIsLogin(0);
+        if (boardDto == null) {
+            boardAddResponse.setIsSuccess(0);
+        } else {
+            boardAddResponse.setIsSuccess(1);
+        }
+        return boardAddResponse;
     }
 }
