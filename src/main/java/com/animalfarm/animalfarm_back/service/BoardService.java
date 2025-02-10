fb88292887891496ea59f7b5cbb6a76381e93e70
hyeokkiyaa <khm38607574@gmail.com>
2025-02-10 17:56:20 +0900

    feat: add from feat from request to entitiy

--- src/main/java/com/animalfarm/animalfarm_back/service/BoardService.java
+++ src/main/java/com/animalfarm/animalfarm_back/service/BoardService.java
@@ -1,10 +1,15 @@
 package com.animalfarm.animalfarm_back.service;
 
+import com.animalfarm.animalfarm_back.controller.request.BoardAddRequest;
+import com.animalfarm.animalfarm_back.controller.response.BoardAddResponse;
+import com.animalfarm.animalfarm_back.domain.Board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
-
+    public BoardAddResponse saveBoard(BoardAddRequest boardAddRequest, String uploadUrl) {
+        BoardDto boardDto = BoardDto.from(boardRepository.save(Board.from(boardAddRequest, uploadUrl)));
+    }
 }
