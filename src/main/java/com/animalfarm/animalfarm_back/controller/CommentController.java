0eaadc539f1303382579b39595ced0deea288e33
hyeokkiyaa <khm38607574@gmail.com>
2025-02-16 17:12:13 +0900

    feat: comment added

--- src/main/java/com/animalfarm/animalfarm_back/controller/CommentController.java
+++ src/main/java/com/animalfarm/animalfarm_back/controller/CommentController.java
@@ -1,11 +1,70 @@
 package com.animalfarm.animalfarm_back.controller;
 
+import com.animalfarm.animalfarm_back.controller.request.board.BoardAddRequest;
+import com.animalfarm.animalfarm_back.controller.request.comment.CommentAddRequest;
+import com.animalfarm.animalfarm_back.controller.response.board.BoardAddResponse;
+import com.animalfarm.animalfarm_back.domain.User;
+import com.animalfarm.animalfarm_back.dto.CommentDto;
+import com.animalfarm.animalfarm_back.service.BoardService;
+import com.animalfarm.animalfarm_back.service.CommentService;
+import com.animalfarm.animalfarm_back.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/comment")
 public class CommentController {
+    private final BoardService boardService;
+    private final CommentService commentService;
+    private final UserService userService;
+    User user = null;
+
+    @PostMapping("/add/{board_id}")
+    public ResponseEntity<BoardAddResponse> addComment(
+            @PathVariable("board_id") Long board_id,
+            @RequestPart("comment") CommentAddRequest commentAddRequest,
+            @RequestParam("image") MultipartFile image,
+            HttpSession session){
+        BoardAddResponse boardAddResponse = new BoardAddResponse();
+        try {
+            boardAddResponse.setIsLogin(loginOrNot(session));
+            if (session.getAttribute("userId") == null) {
+                user = userService.findUserById("1");
+                if (user == null) {
+                    userService.saveOrUpdateUser("1", "익명", "익명", "https://hkwon.s3.ap-northeast-2.amazonaws.com/va/profile.png");
+                    user = userService.findUserById("1");
+                }
+            } else {
+                user = userService.findUserById(session.getAttribute("userId").toString());
+            }
+
+            CommentDto commentDto = commentService.saveComment(commentAddRequest, image, "/va", user);
+            if (commentDto == null) {
+                boardAddResponse.setIsSuccess(0);
+            }
+            else {
+                boardAddResponse.setIsSuccess(1);
+            }
+            return ResponseEntity.ok(boardAddResponse);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            boardAddResponse.setIsSuccess(0);
+            return ResponseEntity.ok(boardAddResponse);
+        }
+
+    }
+
+    private int loginOrNot(HttpSession session) {
+        String userId = (String) session.getAttribute("userId");
+        if (userId == null) {
+            return 0;
+        }
+        return 1;
+    }
 }
--- src/main/java/com/animalfarm/animalfarm_back/service/CommentService.java
+++ src/main/java/com/animalfarm/animalfarm_back/service/CommentService.java
@@ -1,12 +1,46 @@
 package com.animalfarm.animalfarm_back.service;
 
+import com.animalfarm.animalfarm_back.controller.request.comment.CommentAddRequest;
+import com.animalfarm.animalfarm_back.domain.Board;
+import com.animalfarm.animalfarm_back.domain.Comment;
+import com.animalfarm.animalfarm_back.domain.User;
+import com.animalfarm.animalfarm_back.dto.BoardDto;
+import com.animalfarm.animalfarm_back.dto.CommentDto;
 import com.animalfarm.animalfarm_back.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
-
     private final CommentRepository commentRepository;
+    private final S3UploadService s3UploadService;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    public CommentDto saveComment(CommentAddRequest commentAddRequest, MultipartFile image, String dirName, User newUser) throws IOException {
+        String commentImageUrl = "";
+        if (!image.isEmpty()) {
+            File uploadFile = s3UploadService.convert(image)
+                    .orElseThrow(() -> new IOException("MultipartFile -> File 변환 실패"));
+            commentImageUrl = s3UploadService.upload(uploadFile, dirName);
+        }
+
+        Comment comment = Comment.from(commentAddRequest, commentImageUrl, newUser);
+        commentRepository.save(comment);
+        return CommentDto.from(comment, generateImageUrl(comment.getImage()));
+    }
+
+    private String generateImageUrl(String storedFileName) {
+        return "https://" + bucket + ".s3." + region + ".amazonaws.com/" + storedFileName;
+    }
 }
