d99c090301542d9bf10c82d5d52ca30905cdf296
hyeokkiyaa <khm38607574@gmail.com>
2025-02-17 09:42:58 +0900

    feat: update for lost added

--- src/main/java/com/animalfarm/animalfarm_back/controller/request/board/UrlRequest.java
+++ src/main/java/com/animalfarm/animalfarm_back/controller/request/board/UrlRequest.java
@@ -1,4 +1,12 @@
 package com.animalfarm.animalfarm_back.controller.request.board;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UrlRequest {
+    private String url;
 }
